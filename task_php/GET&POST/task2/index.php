c70dddec6d809c06c20e9bed784930c71227ec22
hakkumi <hakkumi1337@gmail.com>
2026-01-27 15:05:42 +0300

    Create index.php

---
+++ task_php/GET&POST/task2/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="/mail.php" method ="$_POST">
+    
+    <input type = "text" name = "num1">
+    <input type = "text" name = "num2">
+    <select name = "select">
+    <option value="value1">+</option>
+    <option value="value2">-</option>
+    <option value="value3">*</option>
+    <option value="value4">/</option>
+    </select>
+    <button type = "submit" value = "отправить">отправить</button>
+
+
+</form>
+</body>
+
+<?
+
+
+
+?>
+
+
+
+
+
+
+</html>
